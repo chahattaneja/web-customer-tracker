f141bfd3603f755ab6f095e1008facc9b267405e
chahattaneja <chahattaeja0107@gmail.com>
2020-09-06 05:05:32 +0530

    added web page for customer list

--- src/com/ct/springdemo/controller/CustomerController.java
+++ src/com/ct/springdemo/controller/CustomerController.java
@@ -1,14 +1,26 @@
 package com.ct.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ct.springdemo.dao.CustomerDAO;
+import com.ct.springdemo.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	@Autowired
+	private CustomerDAO customerDao;
+	
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model model) {
+		List<Customer> customers = customerDao.getCustomers();
+		model.addAttribute("customers", customers);
 		return "list-customers";
 	}
 	
---
+++ src/com/ct/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.ct.springdemo.dao;
+
+import java.util.List;
+
+import com.ct.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	List<Customer> getCustomers();
+
+}
