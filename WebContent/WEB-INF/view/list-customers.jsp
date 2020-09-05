d7f6deafe306e97c50d6e456f7fae3de01f7b3f9
chahattaneja <chahattaeja0107@gmail.com>
2020-09-05 17:49:47 +0530

    project setup and test

---
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Customers List</title>
+
+</head>
+
+<body>
+Customers list coming soon
+</body>
+
+</html>
---
+++ src/com/ct/testdb/TestDbServlet.java
@@ -0,0 +1,47 @@
+package com.ct.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String user = "springstudent";
+		String pass = "springstudent";
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+		String driver = "com.mysql.jdbc.Driver";
+		
+		try {
+			PrintWriter writer = response.getWriter(); 
+			
+			Class.forName(driver);
+			Connection conn = DriverManager.getConnection(jdbcUrl,user,pass);
+			
+			writer.println("Success");
+			
+			conn.close();
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
